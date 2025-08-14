c6a0e22a5374cad9b276938199c841bd3c72c9d2
riteshc852 <riteshc852@users.noreply.github.com>
2025-08-14 23:01:47 +0530

    Commit

--- c_patterns/Star_square.c
+++ c_patterns/Star_square.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    int m;
-    printf("Enter number of columns : ");
+     int n;
+    
+    printf("Enter number : ");
     scanf("%d",&n);
     printf("\n");
-    printf("Enter number of rows : ");
-    scanf("%d",&m);
-    printf("\n");
+   
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             printf("*");
         }
---
+++ c_patterns/Star_triangle_inverted.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+
+    for (int i = 4; i > 0; i--)
+    {
+        printf("*");
+        printf("\n");
+    }
+    
+
+    return 0;
+}
