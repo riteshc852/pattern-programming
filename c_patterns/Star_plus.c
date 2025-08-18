1e0897036cb8979b6475d0b7416680faeb572593
riteshc852 <riteshc852@users.noreply.github.com>
2025-08-18 20:39:47 +0530

    commit

--- c_patterns/Star_plus.c
+++ c_patterns/Star_plus.c
@@ -1,21 +1,26 @@
-// #include <stdio.h>
-
-// int main() {
-//     int n;
-//     printf("Enter Number (odd only) : ");
-//     scanf("%d",&n);
-//     for (int i = 0; i <= 5; i++)
-//     {
-//         for (int j = 0; j < 4; j++)
-//         {
+ #include <stdio.h>
+ int main() {
+     int n;
+     printf("Enter Number (odd only) : ");
+     scanf("%d",&n);
+     int a = (n/2) + 1 ;  // n/2 + 1 = integer for computer 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j==a || i==a) 
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            } 
             
-//         }
-        
-//     }
+        }
+        printf("\n");
+    }
     
-        
+    
+     return 0;
+ }
 
-//     return 0;
-// }
-
-/*To be continued */
